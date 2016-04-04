fe58b83d9a3f0aa384720d79cbc041c37f474498
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-04-05 00:50:23 +0900

    Add imshow for source image for comparison.

--- src/test.cpp
+++ src/test.cpp
@@ -4,15 +4,16 @@
 int
 main(int argc, char* argv[])
     {
-    cv::Mat src = cv::imread("test.png");
+    cv::Mat src = cv::imread(argv[1]);
     GraphSpliter graphSpliter;
 
     graphSpliter.setImage(src);
-   
+
+    cv::imshow("src", src);
     cv::imshow("data", graphSpliter.getDataImage());
     cv::imshow("x axis", graphSpliter.getXAxisImage());
     cv::imshow("y axis", graphSpliter.getYAxisImage());
- 
+
     cv::waitKey(0);
 
     return 0;
