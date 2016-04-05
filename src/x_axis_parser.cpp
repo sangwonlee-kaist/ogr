0dfc7e7f0ba2de8e68fff1e7835b674b6521bd6e
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-04-05 23:07:03 +0900

    Add character region seperator.

--- src/x_axis_parser.cpp
+++ src/x_axis_parser.cpp
@@ -56,7 +56,7 @@ XAxisParser::parseAxis()
 
     PRTIMG(pImpl->axisImage)
 
-    // Remove unwanted xtics.
+    // Remove unwanted xticks.
     // First, Make image to gray color.
     cv::Mat grayImage;
     if (pImpl->axisImage.channels() == 3)
@@ -67,8 +67,110 @@ XAxisParser::parseAxis()
         {
         grayImage = pImpl->axisImage.clone();
         }
-    
+
     PRTIMG(grayImage)
+
+    // Second, Change gray to binary.
+    cv::Mat binaryImage;
+    cv::adaptiveThreshold(~grayImage,
+        binaryImage,
+        255,
+        cv::ADAPTIVE_THRESH_GAUSSIAN_C,
+        cv::THRESH_BINARY_INV,
+        3, // Large kernel size to reduce noise.
+       -3  // Larage value to remove noise.
+        );
+
+    PRTIMG(binaryImage)
+
+    // Seperate image to 3 region.
+    // 1. tic region.
+    // 2. tic value region.
+    // 3. label region.
+    int numRows = pImpl->axisImage.rows;
+
+    // Find first non zero row index.
+    int firstNonZeroRowIndex = 0;
+    for (int rowIndex = 0; rowIndex < numRows; ++rowIndex)
+        {
+        cv::Mat row = binaryImage.row(rowIndex);
+        if (std::count(row.begin<uchar>(), row.end<uchar>(), 0) > 0)
+            {
+            firstNonZeroRowIndex = rowIndex;
+            break;
+            }
+        }
+
+    bool isTickRegion   = true;
+    bool isNumberRegion = false;
+    bool isLabelRegion  = false;
+    bool isEmptyRegion  = false;
+    int numberBeginRowIndex = 0;
+    int numberEndRowIndex   = 0;
+    int labelBeginRowIndex  = 0;
+    int labelEndRowIndex    = numRows;
+
+    for (int rowIndex = firstNonZeroRowIndex; rowIndex < numRows; ++rowIndex)
+        {
+        cv::Mat row = binaryImage.row(rowIndex);
+        int numZeros = std::count(row.begin<uchar>(), row.end<uchar>(), 0);
+
+        if (isTickRegion)
+            {
+            if (not isEmptyRegion && numZeros == 0)
+                {
+                isEmptyRegion = true;
+                }
+            else if (isEmptyRegion && numZeros != 0)
+                {
+                isEmptyRegion = false;
+                isTickRegion  = false;
+                isNumberRegion = true;
+                isLabelRegion  = false;
+                numberBeginRowIndex = rowIndex;
+                }
+            }
+
+        if (isNumberRegion)
+            {
+            if (not isEmptyRegion && numZeros == 0)
+                {
+                isEmptyRegion = true;
+                numberEndRowIndex = rowIndex - 1;
+                }
+            else if (isEmptyRegion && numZeros != 0)
+                {
+                isEmptyRegion = false;
+                isTickRegion  = false;
+                isNumberRegion = false;
+                isLabelRegion  = true;
+                labelBeginRowIndex = rowIndex;
+                }
+            }
+
+        if (isLabelRegion)
+            {
+            // Nohing to do...
+            if (not isEmptyRegion && numZeros == 0)
+                {
+                isEmptyRegion = true;
+                labelEndRowIndex = rowIndex - 1;
+                }
+            }
+        }
+
+    int numCols = pImpl->axisImage.cols;
+    cv::rectangle(pImpl->axisImage,
+             cv::Point(0, numberBeginRowIndex),
+             cv::Point(numCols - 1, numberEndRowIndex),
+             cv::Scalar(0, 0, 255));
+
+    cv::rectangle(pImpl->axisImage,
+             cv::Point(0, labelBeginRowIndex),
+             cv::Point(numCols - 1, labelEndRowIndex),
+             cv::Scalar(0, 0, 255));
+
+    PRTIMG(pImpl->axisImage)
 
     pImpl->isParsed = true;
     }
