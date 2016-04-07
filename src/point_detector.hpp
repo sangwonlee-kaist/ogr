4bc025483a83982d4db7a9d55ff07070a40be7fd
sangwonlee-kaist <lsw91@kaist.ac.kr>
2016-04-08 05:11:25 +0900

    Save some prototype.

--- src/point_detector.hpp
+++ src/point_detector.hpp
@@ -6,7 +6,9 @@ class PointDetector
 public:
      PointDetector();
     ~PointDetector();
+    void setImage(const cv::Mat& dataImage);
     void detect();
+    std::vector<cv::Point> getPoints();
 private:
     class impl;
     std::unique_ptr<impl> pImpl;
--- src/test.cpp
+++ src/test.cpp
@@ -1,25 +1,42 @@
 #include "config.hpp"
 #include "graph_spliter.hpp"
 #include "x_axis_parser.hpp"
+#include "point_detector.hpp"
 
 int
 main(int argc, char* argv[])
 try {
+    if (argc < 2)
+        {
+        std::cout << "Type file name." << std::endl;
+        }
+
     cv::Mat src = cv::imread(argv[1]);
+
+    if (src.empty())
+        {
+        std::cout << "File " << argv[1] << " does not exist." << std::endl;
+        }
+
     GraphSpliter graphSpliter;
 
     graphSpliter.setImage(src);
+    graphSpliter.split();
 
-    cv::imshow("src", src);
-    //cv::imshow("data", graphSpliter.getDataImage());
-    //cv::imshow("x axis", graphSpliter.getXAxisImage());
-    //cv::imshow("y axis", graphSpliter.getYAxisImage());
+    cv::imshow("src image", src);
 
-    XAxisParser xAxisParser;
+    cv::imshow("data", graphSpliter.getDataImage());
+    cv::imshow("x axis", graphSpliter.getXAxisImage());
+    cv::imshow("y axis", graphSpliter.getYAxisImage());
 
-    xAxisParser.setImage(graphSpliter.getXAxisImage());
+    PointDetector pointDetector;
+    pointDetector.setImage(graphSpliter.getDataImage());
+    pointDetector.detect();
+    //XAxisParser xAxisParser;
+    //xAxisParser.setImage(graphSpliter.getXAxisImage());
+    //std::cout << "x label is " << xAxisParser.getLabel() << std::endl;
 
-    std::cout << "x label is " << xAxisParser.getLabel() << std::endl;
+    cv::waitKey(0);
 
     return 0;
     }
